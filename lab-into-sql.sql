c6ff066d0addfd22370a58517e8c4640f6ce02d0
Olga Zakharova <olga.zaharova.11@gmail.com>
2023-05-16 17:51:54 +0200

    first SQL lab completed

---
+++ lab-into-sql.sql
@@ -0,0 +1,7 @@
+USE sakila;
+SELECT * FROM actor, film, customer;
+SELECT title FROM film;
+SELECT name AS language FROM language;
+SELECT count(*) from store;
+SELECT count(*) from staff;
+SELECT first_name from staff; 
